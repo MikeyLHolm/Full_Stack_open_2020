db13a88430eb812185cd5f120e956561cd87f1ba
Mikko Lindholm <mlindhol@c2r6p6.hive.fi>
2020-09-05 15:55:49 +0300

    update cors and pushing to heroku

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 const unknownEndpoint = (request, response) => {
@@ -101,7 +103,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
