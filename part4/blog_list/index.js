0c36103aa82b1b08214134c9018fe4c8b428a3bb
Mikko Lindholm <mlindhol@student.hive.fi>
2020-09-12 23:24:00 +0300

    refactored env and models to work in 4.2

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,53 +1,10 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const { notEqual } = require('assert')
-
-//const password = process.argv[2]
-
-const mongoUrl =
-  'mongodb+srv://lvlsevenwizard:secret777@bloglist.kjyjh.mongodb.net/bloglist?retryWrites=true&w=majority'
-
-console.log('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-/* const blog = new Blog ({
-  title: "30 Algorithms",
-  author: "Iulia",
-  url: "https://dev.to/iuliagroza/complete-introduction-to-the-30-most-essential-data-structures-algorithms-43kd",
-  likes: 1
-})
-
-blog.save().then(result => {
-  console.log('blog saved')
-  mongoose.connection.close()
-}) */
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
@@ -74,7 +31,8 @@ app.post('/api/blogs', (request, response) => {
     // })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
