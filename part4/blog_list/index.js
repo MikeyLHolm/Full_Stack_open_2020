ead06bf2cada62c678d43e6ee9bee575bfa16968
Mikko Lindholm <mlindhol@student.hive.fi>
2020-09-12 22:39:50 +0300

    4.1 done

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -3,6 +3,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const { notEqual } = require('assert')
+
+//const password = process.argv[2]
+
+const mongoUrl =
+  'mongodb+srv://lvlsevenwizard:secret777@bloglist.kjyjh.mongodb.net/bloglist?retryWrites=true&w=majority'
+
+console.log('connecting to', mongoUrl)
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -11,10 +27,27 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+/* const blog = new Blog ({
+  title: "30 Algorithms",
+  author: "Iulia",
+  url: "https://dev.to/iuliagroza/complete-introduction-to-the-30-most-essential-data-structures-algorithms-43kd",
+  likes: 1
+})
+
+blog.save().then(result => {
+  console.log('blog saved')
+  mongoose.connection.close()
+}) */
 
 app.use(cors())
 app.use(express.json())
@@ -23,7 +56,7 @@ app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
+      response.json(blogs.map(blog => blog.toJSON()))
     })
 })
 
@@ -32,9 +65,13 @@ app.post('/api/blogs', (request, response) => {
 
   blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
+    .then(savedBlog => savedBlog.toJSON())
+    .then(savedAndFormatedBlog => {
+      response.json(savedAndFormatedBlog)
     })
+    // .then(result => {
+    //   response.status(201).json(result)
+    // })
 })
 
 const PORT = 3003
