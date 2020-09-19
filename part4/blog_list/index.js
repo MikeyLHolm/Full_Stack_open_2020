947874ee60cafe7b05c2108884e0316990425e28
Mikko Lindholm <mlindhol@student.hive.fi>
2020-09-19 19:57:03 +0300

    4.2 OK

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,38 +1,10 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const { notEqual } = require('assert')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(savedBlog => savedBlog.toJSON())
-    .then(savedAndFormatedBlog => {
-      response.json(savedAndFormatedBlog)
-    })
-    // .then(result => {
-    //   response.status(201).json(result)
-    // })
-})
-
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/blog_list/models/blog.js
+++ part4/blog_list/models/blog.js
@@ -3,18 +3,6 @@ const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
-const mongoUrl = process.env.MONGODB_URI
-
-console.log('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
