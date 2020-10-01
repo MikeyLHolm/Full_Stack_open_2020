819b4a54ec9bc2dd26ec021391fbd32f80e01055
Mikko Lindholm <mlindhol@c2r6p3.hive.fi>
2020-10-01 12:44:49 +0300

    4.4 done, hardcoded totallikes

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,21 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (array) => {
+  const reducer = (sum, { likes } ) => {
+    return sum + likes
+  }
+
+  if (array.length === 0)
+    return 0
+  else if (array.length === 1)
+    return array[0].likes
+  else
+    return array.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
