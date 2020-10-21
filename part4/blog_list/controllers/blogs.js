0013946a8a6f72a35599460104f1a837fe811679
Mikko Lindholm <mikko.lindholm3@gmail.com>
2020-10-21 17:52:53 +0300

    4.19: bloglist expansion, step7: OK

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -18,7 +27,18 @@ blogsRouter.post('/', async (request, response) => {
       error: 'No title and body!'
     })
 
-  const user = await User.findById(body.user)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+  const user = await User.findById(decodedToken.id)
+
+
+  //const user = await User.findById(body.user)
 
   const blog = new Blog({
     url: body.url,
