80e48b42ff1c36fd441cee350daafc4c88b049df
Mikko Lindholm <mikko.lindholm3@gmail.com>
2020-10-22 09:45:59 +0300

    4.20*: bloglist expansion, step8: OK

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -27,18 +27,14 @@ blogsRouter.post('/', async (request, response) => {
       error: 'No title and body!'
     })
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({
       error: 'token missing or invalid'
     })
   }
   const user = await User.findById(decodedToken.id)
-
-
-  //const user = await User.findById(body.user)
 
   const blog = new Blog({
     url: body.url,
